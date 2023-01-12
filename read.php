8e7c2173dd8ea0b77326580a39c7c630a08c264f
Waayway <thijs@vanwaaij.com>
2023-01-12 13:45:59 +0100

    tabel nu klaar

--- read.php
+++ read.php
@@ -1,14 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <?php 
-    
+    <?php
+    require("connect.php");
+
+    $sql = "SELECT id, merk, model, topsnelheid, prijs FROM DureAuto ORDER BY prijs";
     ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Merk</th>
+                <th>Model</th>
+                <th>Topsnelheid</th>
+                <th>Prijs</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($pdo->query($sql, PDO::FETCH_OBJ) as $obj) {
+                echo "<tr>";
+                echo "<td>" . $obj->merk . "</td>";
+                echo "<td>" . $obj->model . "</td>";
+                echo "<td>" . $obj->topsnelheid . "</td>";
+                echo "<td>" . $obj->prijs . "</td>";
+                echo "<td><a href=\"delete.php?id=" . $obj->id "\">Delete</a></td>"
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
+
 </html>
